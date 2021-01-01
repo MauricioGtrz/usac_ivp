e766c503eaffb4d21ca28d8ed953507e8be2db30
MauricioGtrz <mauriciogtrz@g.ucla.edu>
2020-12-31 18:21:33 -0800

    removed the events page on mount function

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,15 +1,11 @@
 import React, { Component } from "react";
-import $ from "jquery";
+// import $ from "jquery";
 
 class events extends Component {
-  componentDidMount() {
-    console.log($(".name").val());
-  }
   render() {
     return (
       <div>
         <h1> Instagram Feed </h1>
-        <input className="name" value="Mau" />
       </div>
     );
   }
